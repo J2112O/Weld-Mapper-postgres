9b3921de2eb3632c1579a3609de4d2501235020b
Joshua Otwell <parabollus@gmail.combigdaddy@LE2>
2017-03-14 18:57:38 -0500

    Working on Db Columns and Creates
    Working on Db Column constants and all the table creates

---
+++ db_column_cons.py
@@ -0,0 +1,36 @@
+attributes_table = "attributes"
+weld_table = "weld"
+bend_table = "bend"
+cmb_bend_table = "combo_bend"
+
+# Common Attributes table columns here
+ca_uid = "id"
+whole_station = "whole_station"
+offset_station = "offset_station"
+gps_point = "gps_shot"
+grade_point = "grade_shot"
+depth_cover = "cover"
+jottings = "notes"
+
+# Bend Table columns here
+bnd_uid = "id"
+deg = "degree"
+bnd_dir = "direction"
+bnd_type = "type"
+
+# ComboBend Table columns here
+cmbo_uid = "id"
+deg2 = "degree_2"
+bnd_dir2 = "direction_2"
+
+# Weld Table columns here
+weld_id = "id"
+wld_type = "type"
+wld_x_id = "weld_id"
+upstream_jt = "upstream_joint"
+downstream_jt = "downstream_joint"
+ah_length = "length_ahead"
+ht = "heat"
+wll_chng = "wall_change"
+ditch_loc = "loation"
+welder_initials = "welder_initials"
--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 import psycopg2 as pg
 import survey_codes as sc
 import helper_functions as hf
+import db_column_cons as colu
+
+conn = pg.connect("dbname=delta user=postgres password=Narmar123 port=5433")
+
+cur = conn.cursor()
+
+# Creating the Common Attributes table here.
+cur.execute("CREATE TABLE IF NOT EXISTS {} ({} SERIAL PRIMARY KEY, {} INTEGER NOT NULL,"
+            "{} REAL NOT NULL, {} INTEGER NOT NULL, {} INTEGER NOT NULL,"
+            "{} REAL NOT NULL, {} VARCHAR(25) DEFAULT 'N/A');").format(colu.attributes_table, colu.ca_uid,
+                                                                       colu.whole_station, colu.offset_station,
+                                                                       colu.gps_point, colu.grade_point,
+                                                                       colu.depth_cover, colu.jottings)
+
+# Creating the Bend Table here.
+cur.execute("CREATE TABLE IF NOT EXISTS {} ({} SERIAL PRIMARY KEY "
+            "REFERENCES {}({}), {} REAL NOT NULL,"
+            "{} VARCHAR(25) NOT NULL, {} VARCHAR(25));").format(colu.bend_table, colu.bnd_uid,
+                                                                colu.attributes_table, colu.ca_uid,
+                                                                colu.deg, colu.bnd_dir, colu.bnd_type)
+
+# Creating the ComboBend Table here.
+cur.execute("CREATE TABLE {} ({} SERIAL PRIMARY KEY REFERENCES {}({}) , {} REAL NOT NULL, "
+            "{} VARCHAR(25));").format(colu.cmb_bend_table, colu.cmbo_uid,
+                                       colu.attributes_table, colu.ca_uid,
+                                       colu.deg2, colu.bnd_dir2)
